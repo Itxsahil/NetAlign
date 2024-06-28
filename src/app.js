8ce5f92a8159b41e14d5fd9aa6833d4b48f4c2b1
itxsahil <sahilkh9087@gmail.com>
2024-06-28 21:28:55 +0530

    add looger

--- src/app.js
+++ src/app.js
@@ -1,34 +1,62 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import logger from "./utils/logger.js";
+import morgan from "morgan";
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS,
     credentials: true,
-   
-}));
+  })
+);
+const morganFormat = ":method :url :status :response-time ms";
 
-app.use(cookieParser({
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
+app.use(
+  cookieParser({
     httpOnly: true,
     sameSite: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: process.env.NODE_ENV === "production",
     maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
-}));
+  })
+);
 
-app.use(express.json({
-    limit: '50mb',
-}));
+app.use(
+  express.json({
+    limit: "50mb",
+  })
+);
 
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: true,
-    limit: '50mb',
-}));
+    limit: "50mb",
+  })
+);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-import userRouter from './routers/Auth/user.routes.js'
+import healthRouter from "./routers/healthcheck/healthcheck.routes.js";
+import userRouter from "./routers/Auth/user.routes.js";
 
-app.use('/api/v1/users', userRouter)
+app.use("/api/v1/health", healthRouter);
+app.use("/api/v1/users", userRouter);
 
-export {app}
+export { app };
