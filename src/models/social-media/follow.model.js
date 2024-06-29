fe0c456eaea0fac0088f717ebbc3d7434b82de09
itxsahil <sahilkh9087@gmail.com>
2024-06-29 10:14:45 +0530

    prettiering follow model

--- src/models/social-media/follow.model.js
+++ src/models/social-media/follow.model.js
@@ -1,20 +1,19 @@
-import mongoose, { Schema } from 'mongoose';
-
+import mongoose, { Schema } from "mongoose";
 
 const followSchema = new Schema(
-    {
-        following: {
-            type: mongoose.Types.ObjectId,
-            ref: 'User',
-        },
-        followedBy: {
-            type: mongoose.Types.ObjectId,
-            ref: 'User',
-        },
+  {
+    following: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
     },
-    {
-        timestamps: true,
-    }
-)
+    followedBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export const Follow = mongoose.model('Follow', followSchema);
+export const Follow = mongoose.model("Follow", followSchema);
