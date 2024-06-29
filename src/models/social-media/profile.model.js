babf830734cacba322cd655ffee64d134ef13f3b
itxsahil <sahilkh9087@gmail.com>
2024-06-29 09:09:09 +0530

    add prettier conf to profile model

--- src/models/social-media/profile.model.js
+++ src/models/social-media/profile.model.js
@@ -1,50 +1,50 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const profileSchema = new Schema(
-    {
-        coverImage: {
-            type: String,
-            required: true,
-            default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr2x24cDdKDm2jZHhbOZtVlbCF8vFVa82Hiw&s"
-        },
-        firstName: {
-            type: String,
-            default: "Jhon",
-        },
-        lastName: {
-            type: String,
-            default: "Doe",
-        },
-        bio : {
-            type: String,
-            default: "",
-        },
-        dob: {
-            type: Date,
-            default: null,
-        },
-        location: {
-            type: String,
-            default: "",
-        },
-        countryCode: {
-            type: String,
-            default: "",
-        },
-        phoneNumber: {
-            type: String,
-            default: "",
-        },
-        owner: {
-            type: Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        }
+  {
+    coverImage: {
+      type: String,
+      required: true,
+      default:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr2x24cDdKDm2jZHhbOZtVlbCF8vFVa82Hiw&s",
     },
-    {
-        timestamps: true,
-    }
+    firstName: {
+      type: String,
+      default: "Jhon",
+    },
+    lastName: {
+      type: String,
+      default: "Doe",
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    dob: {
+      type: Date,
+      default: null,
+    },
+    location: {
+      type: String,
+      default: "",
+    },
+    countryCode: {
+      type: String,
+      default: "",
+    },
+    phoneNumber: {
+      type: String,
+      default: "",
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-
-export const Profile = mongoose.model('Profile',profileSchema);
+export const Profile = mongoose.model("Profile", profileSchema);
